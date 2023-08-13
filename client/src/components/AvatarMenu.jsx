17fa6610850645512f01418312b99fc219a7c555
codingmetta <63598116+codingmetta@users.noreply.github.com>
2023-08-13 10:54:16 +0200

    minor changes

--- client/src/components/AvatarMenu.jsx
+++ client/src/components/AvatarMenu.jsx
@@ -1,12 +1,11 @@
 'use client';
 import { Avatar } from 'flowbite-react';
-import LoginModalBtn from './subcomponents/LoginModalBtn';
 
 export default function AvatarMenu() {
     return (
         <>
-            {/*Dropdown Menu*/}
-            <div id="costum-dropdown">
+            {/*Dropdown Button*/}
+            <div id="costumDropdown">
                 <button id="dropdownNavbarLink" data-dropdown-toggle="dropdownNavbar" data-dropdown-offset-distance="10" data-dropdown-offset-skidding="-75" className="flex items-center justify-between rounded-full border md:border shadow-none hover:shadow-md focus:shadow-md md:mr-0 md:hover:bg-transparent md:p-0 md:w-auto dark:text-white dark:border-gray-700 dark:hover:bg-gray-700 md:dark:hover:bg-transparent  dark:hover:ring-gray-600  dark:hover:shadow-md">
                     <div className="flex items-center flex-wrap pl-3 pr-0 pt-0 pb-0 gap-2">
                         <svg xmlns="http://www.w3.org/2000/svg" className="w-4 h-4 fill-none stroke-black scale-125" strokeWidth={2} viewBox="0 0 24 24">
@@ -16,13 +15,18 @@ export default function AvatarMenu() {
                     </div>
                 </button>
 
+                {/*Dropdown Menu*/}
                 <div id="dropdownNavbar" className="z-10  tracking-wide text-sm font-extralight hidden bg-white divide-y divide-gray-100 rounded-xl shadow-[0px_0px_14px_0px_rgba(0,0,0,0.1)] w-56 dark:bg-gray-700 dark:divide-gray-600">
                     <ul className="py-2 text-sm text-gray-700 dark:text-gray-400 space-y-1" aria-labelledby="dropdownLargeButton">
                         <li>
                             <a href="#" className="block px-4 py-2 font-medium hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Registrieren</a>
                         </li>
+
+                        {/*Element toggles modal Login*/}
                         <li>
-                            <LoginModalBtn />
+                            <label data-modal-target="defaultModal" data-modal-toggle="defaultModal" className="text-sm text-gray-700 block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white" type="button">
+                                Einloggen
+                            </label>
                         </li>
                     </ul>
 
