e3c69037bae77f14c07437e0208e3e6b2244a138
codingmetta <t.deckardt@outlook.de>
2023-09-01 18:26:36 +0200

    Accordion added

--- client/src/pages/HostStartPage.jsx
+++ client/src/pages/HostStartPage.jsx
@@ -136,10 +136,125 @@ export default function HostStartPage() {
 
             <footer className="p-6 bg-gray-200 items-center flex flex-col">
 
-                <div className="upper-footer border flex flex-col">
+                <div className="upper-footer border flex flex-col w-3/4">
                     <div className="flex flex-row border border-gray-800">
-                        <div className="border border-gray-800">Antworten auf deine Fragen</div>
-                        <div className="border border-gray-800">Liste FAQ</div>
+
+                        <div className="border border-gray-800 text-4xl w-1/2">
+                            Antworten auf deine Fragen
+                        </div>
+
+                        {/*Accordion*/}
+                        <div className="border border-gray-800 w-1/2 text-2xl">
+
+                            <div id="accordion-flush" data-accordion="collapse" data-active-classes="bg-white dark:bg-gray-900 text-gray-900 dark:text-white" data-inactive-classes="text-gray-500 dark:text-gray-400">
+                                <h2 id="accordion-flush-heading-1">
+                                    <button type="button" className="flex items-center justify-between w-full py-5 font-medium text-left text-gray-500 border-b border-gray-200 dark:border-gray-700 dark:text-gray-400" data-accordion-target="#accordion-flush-body-1" aria-expanded="true" aria-controls="accordion-flush-body-1">
+                                        <span className="font-light">
+                                            Eignet sich meine Unterkunft für Airbnb?
+                                        </span>
+                                        <svg data-accordion-icon className="w-3 h-3 rotate-180 shrink-0" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
+                                            <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M9 5 5 1 1 5" />
+                                        </svg>
+                                    </button>
+                                </h2>
+
+                                <div id="accordion-flush-body-1" className="hidden" aria-labelledby="accordion-flush-heading-1">
+                                    <div class="py-5 border-b border-gray-200 dark:border-gray-700">
+                                        <p className="mb-2 text-gray-500 dark:text-gray-400">
+                                            Flowbite is an open-source library of interactive components built on top of Tailwind CSS including buttons, dropdowns, modals, navbars, and more.
+                                        </p>
+                                        <p className="text-gray-500 dark:text-gray-400">
+                                            Check out this guide to learn how to <a href="/docs/getting-started/introduction/" class="text-blue-600 dark:text-blue-500 hover:underline">get started</a> and start developing websites even faster with components on top of Tailwind CSS.</p>
+                                    </div>
+                                </div>
+
+                                <h2 id="accordion-flush-heading-2">
+                                    <button type="button" className="flex items-center justify-between w-full py-5 font-medium text-left text-gray-500 border-b border-gray-200 dark:border-gray-700 dark:text-gray-400" data-accordion-target="#accordion-flush-body-2" aria-expanded="false" aria-controls="accordion-flush-body-2">
+                                    <span className="font-light">
+                                            Muss ich ständig Gäste aufnehmen?
+                                        </span>
+                                        <svg data-accordion-icon className="w-3 h-3 rotate-180 shrink-0" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
+                                            <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M9 5 5 1 1 5" />
+                                        </svg>
+                                    </button>
+                                </h2>
+
+                                <div id="accordion-flush-body-2" className="hidden" aria-labelledby="accordion-flush-heading-2">
+                                    <div className="py-5 border-b border-gray-200 dark:border-gray-700">
+                                        <p className="mb-2 text-gray-500 dark:text-gray-400">Flowbite is first conceptualized and designed using the Figma software so everything you see in the library has a design equivalent in our Figma file.</p>
+                                        <p className="text-gray-500 dark:text-gray-400">Check out the <a href="https://flowbite.com/figma/" className="text-blue-600 dark:text-blue-500 hover:underline">Figma design system</a> based on the utility classes from Tailwind CSS and components from Flowbite.</p>
+                                    </div>
+                                </div>
+
+                                <h2 id="accordion-flush-heading-3">
+                                    <button type="button" className="flex items-center justify-between w-full py-5 font-medium text-left text-gray-500 border-b border-gray-200 dark:border-gray-700 dark:text-gray-400" data-accordion-target="#accordion-flush-body-3" aria-expanded="false" aria-controls="accordion-flush-body-3">
+                                    <span className="font-light">
+                                            Wie intensiv soll ich mit Gästen interagieren?
+                                        </span>
+                                        <svg data-accordion-icon class="w-3 h-3 rotate-180 shrink-0" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
+                                            <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M9 5 5 1 1 5" />
+                                        </svg>
+                                    </button>
+                                </h2>
+                                <div id="accordion-flush-body-3" className="hidden" aria-labelledby="accordion-flush-heading-3">
+                                    <div className="py-5 border-b border-gray-200 dark:border-gray-700">
+                                        <p className="mb-2 text-gray-500 dark:text-gray-400">The main difference is that the core components from Flowbite are open source under the MIT license, whereas Tailwind UI is a paid product. Another difference is that Flowbite relies on smaller and standalone components, whereas Tailwind UI offers sections of pages.</p>
+                                        <p className="mb-2 text-gray-500 dark:text-gray-400">However, we actually recommend using both Flowbite, Flowbite Pro, and even Tailwind UI as there is no technical reason stopping you from using the best of two worlds.</p>
+                                        <p className="mb-2 text-gray-500 dark:text-gray-400">Learn more about these technologies:</p>
+                                        <ul className="pl-5 text-gray-500 list-disc dark:text-gray-400">
+                                            <li><a href="https://flowbite.com/pro/" className="text-blue-600 dark:text-blue-500 hover:underline">Flowbite Pro</a></li>
+                                            <li><a href="https://tailwindui.com/" rel="nofollow" className="text-blue-600 dark:text-blue-500 hover:underline">Tailwind UI</a></li>
+                                        </ul>
+                                    </div>
+                                </div>
+                                <h2 id="accordion-flush-heading-4">
+                                    <button type="button" className="flex items-center justify-between w-full py-5 font-medium text-left text-gray-500 border-b border-gray-200 dark:border-gray-700 dark:text-gray-400" data-accordion-target="#accordion-flush-body-3" aria-expanded="false" aria-controls="accordion-flush-body-4">
+                                    <span className="font-light">
+                                            Welche Tipps gibt es dafür, ein:e großartige:r Gastgeber:in auf Airbnb zu sein?
+                                        </span>
+                                        <svg data-accordion-icon class="w-3 h-3 rotate-180 shrink-0" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
+                                            <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M9 5 5 1 1 5" />
+                                        </svg>
+                                    </button>
+                                </h2>
+                                <div id="accordion-flush-body-4" className="hidden" aria-labelledby="accordion-flush-heading-3">
+                                    <div className="py-5 border-b border-gray-200 dark:border-gray-700">
+                                        <p className="mb-2 text-gray-500 dark:text-gray-400">The main difference is that the core components from Flowbite are open source under the MIT license, whereas Tailwind UI is a paid product. Another difference is that Flowbite relies on smaller and standalone components, whereas Tailwind UI offers sections of pages.</p>
+                                        <p className="mb-2 text-gray-500 dark:text-gray-400">However, we actually recommend using both Flowbite, Flowbite Pro, and even Tailwind UI as there is no technical reason stopping you from using the best of two worlds.</p>
+                                        <p className="mb-2 text-gray-500 dark:text-gray-400">Learn more about these technologies:</p>
+                                        <ul className="pl-5 text-gray-500 list-disc dark:text-gray-400">
+                                            <li><a href="https://flowbite.com/pro/" className="text-blue-600 dark:text-blue-500 hover:underline">Flowbite Pro</a></li>
+                                            <li><a href="https://tailwindui.com/" rel="nofollow" className="text-blue-600 dark:text-blue-500 hover:underline">Tailwind UI</a></li>
+                                        </ul>
+                                    </div>
+                                </div>
+
+                                <h2 id="accordion-flush-heading-5">
+                                    <button type="button" className="flex items-center justify-between w-full py-5 font-medium text-left text-gray-500 border-b border-gray-200 dark:border-gray-700 dark:text-gray-400" data-accordion-target="#accordion-flush-body-3" aria-expanded="false" aria-controls="accordion-flush-body-4">
+                                    <span className="font-light">
+                                            Welche Gebühren berechnet Airbnb?
+                                        </span>
+                                        <svg data-accordion-icon class="w-3 h-3 rotate-180 shrink-0" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
+                                            <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M9 5 5 1 1 5" />
+                                        </svg>
+                                    </button>
+                                </h2>
+                                <div id="accordion-flush-body-5" className="hidden" aria-labelledby="accordion-flush-heading-3">
+                                    <div className="py-5 border-b border-gray-200 dark:border-gray-700">
+                                        <p className="mb-2 text-gray-500 dark:text-gray-400">The main difference is that the core components from Flowbite are open source under the MIT license, whereas Tailwind UI is a paid product. Another difference is that Flowbite relies on smaller and standalone components, whereas Tailwind UI offers sections of pages.</p>
+                                        <p className="mb-2 text-gray-500 dark:text-gray-400">However, we actually recommend using both Flowbite, Flowbite Pro, and even Tailwind UI as there is no technical reason stopping you from using the best of two worlds.</p>
+                                        <p className="mb-2 text-gray-500 dark:text-gray-400">Learn more about these technologies:</p>
+                                        <ul className="pl-5 text-gray-500 list-disc dark:text-gray-400">
+                                            <li><a href="https://flowbite.com/pro/" className="text-blue-600 dark:text-blue-500 hover:underline">Flowbite Pro</a></li>
+                                            <li><a href="https://tailwindui.com/" rel="nofollow" className="text-blue-600 dark:text-blue-500 hover:underline">Tailwind UI</a></li>
+                                        </ul>
+                                    </div>
+                                </div>
+                            </div>
+
+
+
+                        </div>
                     </div>
                     <div className="border border-gray-800">Banner</div>
 
